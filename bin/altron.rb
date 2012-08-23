8a786350268bdf05c0f6ca85f7c39c19c88153d7
Alison Jones <alison.k.jones@gmail.com>
2012-08-23 14:15:30 -0600

    new bot

---
+++ bin/altron.rb
@@ -0,0 +1,43 @@
+################################################################################
+
+# freeplay --host 66.175.219.57 altron.rb
+
+class AlTron < Freeplay::Player
+
+  ##############################################################################
+  def move
+    x, y = nil, nil
+
+    # First attempt
+    if board.last_opponent_move
+      logger.info("1: searching for an open top/right adjacent space")
+
+      allowed = board.adjacent(*board.last_opponent_move)
+      match = allowed.find_all{|(ax, ay)|board[ax, ay] == :empty}
+      x, y = match.sort.last
+      end
+
+    #second try
+    if x.nil? or y.nil?
+      logger.info("2: searching for first available space")
+
+      x, y = catch(:found_empty_space) do
+        board.size.times do |bx|
+          board.size.times do |by|
+            throw(:found_empty_space, [bx, by]) if board[bx, by] == :empty
+          end
+        end
+      end
+    end
+    
+    [x, y]
+    
+  end 
+end
+
+
+
+
+
+
+
